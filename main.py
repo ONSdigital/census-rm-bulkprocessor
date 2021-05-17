2c4f81b8c20895c5c93f24473f827ffa3c4b09de
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-05-17 12:48:02 +0100

    Added feature to display jobs, plus use stubbed API

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
 
-from flask import Flask, flash, request, redirect, Response
+from flask import Flask, flash, request, redirect, Response, jsonify
 from werkzeug.utils import secure_filename
-
 
 app = Flask(__name__,
             static_url_path='',
@@ -48,3 +47,20 @@ def upload_file():
       <input type=submit value=Upload>
     </form>
     '''
+
+
+@app.route('/job', methods=['GET'])
+def get_jobs():
+    jobs = [{"id": "19864513-eca0-4d34-860e-4b71d9fc03af", "bulkProcess": "NEW_ADDRESS",
+             "createdAt": "2021-05-17T11:32:10.106716+01:00", "lastUpdatedAt": "2021-05-17T11:32:11.631205+01:00",
+             "fileName": "1_per_treatment_code.csv", "jobStatus": "FILE_UPLOADED", "rowCount": 47, "rowErrorCount": 0,
+             "fatalErrorDescription": None},
+            {"id": "8f0fde6c-d7c1-4eb0-a491-c79f993aad69", "bulkProcess": "NEW_ADDRESS",
+             "createdAt": "2021-05-17T11:32:18.825215+01:00", "lastUpdatedAt": "2021-05-17T11:32:19.953604+01:00",
+             "fileName": "goihdsfgdfslgndf.csv", "jobStatus": "PROCESSED_OK", "rowCount": 47, "rowErrorCount": 0,
+             "fatalErrorDescription": None},
+            {"id": "8f0fde6c-d7c1-4eb0-a491-c79f993aad69", "bulkProcess": "NEW_ADDRESS",
+             "createdAt": "2021-05-17T12:12:12.123+01:00", "lastUpdatedAt": "2021-05-17T11:32:19.953604+01:00",
+             "fileName": "ertoerhtgrg.csv", "jobStatus": "PROCESSED_TOTAL_FAILURE", "rowCount": 47, "rowErrorCount": 0,
+             "fatalErrorDescription": None}]
+    return jsonify(jobs)
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,68 +1,18 @@
 import React, { Component } from 'react';
-import '@fontsource/roboto';
-import axios from 'axios';
-import { Button, Box, Grid, Paper, AppBar, Toolbar, Typography, LinearProgress, Snackbar, SnackbarContent, Dialog, DialogContent } from '@material-ui/core';
+import { Box, Typography, AppBar, Toolbar } from '@material-ui/core';
+import ProcessDetails from './ProcessDetails'
 
 class App extends Component {
   constructor(props) {
     super(props)
-
-    // The state holds any values which should initiate a re-rendering of the UI, if they change
-    this.state = {
-      fileProgress: 0,  // Percentage of the file uploaded
-      fileUploadSuccess: false, // Flag to flash the snackbar message on the screen, when file uploads successfully
-      uploadInProgress: false // Flag to display the file upload progress modal dialog
-    }
   }
 
-  handleUpload = (e) => {
-    // Display the progress modal dialog
-    this.setState({
-      uploadInProgress: true,
-    })
-
-    const formData = new FormData();
-    formData.append("file", e.target.files[0]);
-
-    // Send the file data to the backend
-    axios.request({
-      method: "post",
-      url: "/upload",
-      data: formData,
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      },
-      onUploadProgress: (p) => {
-        console.log(p);
-
-        // Update file upload progress
-        this.setState({
-          fileProgress: p.loaded / p.total
-        })
-      }
-
-    }).then(data => {
-      // Hide the progress dialog and flash the snackbar message
-      this.setState({
-        fileProgress: 1.0,
-        fileUploadSuccess: true,
-        uploadInProgress: false,
-      })
-    })
-  }
-
-  handleClose = (event, reason) => {
-    // Ignore clickaways so that the dialog is modal
-    if (reason === 'clickaway') {
-      return;
-    }
-
-    this.setState({
-      fileUploadSuccess: false,
-    })
-  }
 
   render() {
+    const processDetails = (
+      <ProcessDetails bulkProcess={'NEW_ADDRESS'}></ProcessDetails>
+    )
+
     return (
       <Box>
         <AppBar position="static">
@@ -72,53 +22,7 @@ class App extends Component {
             </Typography>
           </Toolbar>
         </AppBar>
-        <Grid>
-          <Paper elevation={3} style={{ margin: 10, padding: 10 }}>
-            <Typography variant="h8" color="inherit" style={{ margin: 10, padding: 10 }}>
-              Please upload a bulk file for processing
-            </Typography>
-            <input
-              accept="csv/*"
-              style={{ display: 'none' }}
-              id="contained-button-file"
-              type="file"
-              onChange={(e) => {
-                this.handleUpload(e)
-              }}
-            />
-            <label htmlFor="contained-button-file">
-              <Button variant="contained" component="span">
-                Upload
-              </Button>
-            </label>
-          </Paper>
-        </Grid>
-        <Dialog open={this.state.uploadInProgress}>
-          <DialogContent style={{ padding: 30 }}>
-            <Typography variant="h6" color="inherit">
-              Uploading file...
-            </Typography>
-            <LinearProgress
-              variant="determinate"
-              value={this.state.fileProgress * 100}
-              style={{ marginTop: 20, marginBottom: 20, width: 400 }} />
-            <Typography variant="h6" color="inherit">
-              {Math.round(this.state.fileProgress * 100)}%
-          </Typography>
-          </DialogContent>
-        </Dialog>
-        <Snackbar
-          open={this.state.fileUploadSuccess}
-          autoHideDuration={6000}
-          onClose={this.handleClose}
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left',
-          }}>
-          <SnackbarContent style={{ backgroundColor: '#4caf50' }}
-            message={'File upload successful!'}
-          />
-        </Snackbar>
+        {processDetails}
       </Box>
     )
   }
