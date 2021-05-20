b49ea39a15da1ec093ec6a9d7c16d0ed2388f2de
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-05-20 11:22:27 +0100

    Add unit tests; fix docker dev

---
+++ src/test/java/uk/gov/ons/census/bulkprocessor/validation/AlphanumericRuleTest.java
@@ -0,0 +1,10 @@
+package uk.gov.ons.census.bulkprocessor.validation;
+
+import org.junit.Test;
+
+public class AlphanumericRuleTest {
+  @Test
+  public void testCheckValidity() {
+    AlphanumericRule underTest = new AlphanumericRule();
+  }
+}
