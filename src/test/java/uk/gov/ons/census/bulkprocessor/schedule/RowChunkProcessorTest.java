8301fbf29970e1c931d67aa1aa9dcfe5bd893c3f
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-05-20 15:05:12 +0100

    Add final unit test

---
+++ src/test/java/uk/gov/ons/census/bulkprocessor/schedule/RowChunkProcessorTest.java
@@ -0,0 +1,100 @@
+package uk.gov.ons.census.bulkprocessor.schedule;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import uk.gov.ons.census.bulkprocessor.model.dto.CreateCaseSample;
+import uk.gov.ons.census.bulkprocessor.model.entity.BulkProcess;
+import uk.gov.ons.census.bulkprocessor.model.entity.Job;
+import uk.gov.ons.census.bulkprocessor.model.entity.JobRow;
+import uk.gov.ons.census.bulkprocessor.model.entity.JobRowStatus;
+import uk.gov.ons.census.bulkprocessor.model.repository.JobRowRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RowChunkProcessorTest {
+  @Mock private JobRowRepository jobRowRepository;
+
+  @Mock private RabbitTemplate rabbitTemplate;
+
+  @InjectMocks private RowChunkProcessor underTest;
+
+  @Test
+  public void testStageChunk() {
+    Job job = new Job();
+    job.setBulkProcess(BulkProcess.NEW_ADDRESS);
+
+    Map<String, String> rowData = new HashMap<>();
+    rowData.put("ADDRESS_LINE1", "test address line 1");
+    rowData.put("UPRN", "test uprn");
+    rowData.put("POSTCODE", "test postcode");
+    rowData.put("ADDRESS_TYPE", "HH");
+    rowData.put("CE_EXPECTED_CAPACITY", "666");
+    rowData.put("CE_SECURE", "1");
+
+    JobRow jobRow = new JobRow();
+    jobRow.setRowData(rowData);
+
+    when(jobRowRepository.findTop10ByJobAndAndJobRowStatus(job, JobRowStatus.STAGED))
+        .thenReturn(List.of(jobRow));
+
+    underTest.processChunk(job);
+
+    ArgumentCaptor<CreateCaseSample> sampleArgumentCaptor =
+        ArgumentCaptor.forClass(CreateCaseSample.class);
+    verify(rabbitTemplate)
+        .convertAndSend(eq(""), eq("case.sample.inbound"), sampleArgumentCaptor.capture());
+    assertThat(sampleArgumentCaptor.getValue().getAddressLine1()).isEqualTo("test address line 1");
+
+    ArgumentCaptor<JobRow> jobRowArgumentCaptor = ArgumentCaptor.forClass(JobRow.class);
+    verify(jobRowRepository).saveAndFlush(jobRowArgumentCaptor.capture());
+    assertThat(jobRowArgumentCaptor.getValue()).isEqualTo(jobRow);
+    assertThat(jobRowArgumentCaptor.getValue().getJobRowStatus())
+        .isEqualTo(JobRowStatus.PROCESSED_OK);
+  }
+
+  @Test
+  public void testStageChunkFailedValidation() {
+    Job job = new Job();
+    job.setBulkProcess(BulkProcess.NEW_ADDRESS);
+
+    Map<String, String> rowData = new HashMap<>();
+    rowData.put("ADDRESS_LINE1", "test address line 1");
+    rowData.put("UPRN", "test uprn");
+    rowData.put("POSTCODE", "%%$*&$£&^$£*");
+    rowData.put("ADDRESS_TYPE", "HH");
+    rowData.put("CE_EXPECTED_CAPACITY", "666");
+    rowData.put("CE_SECURE", "1");
+
+    JobRow jobRow = new JobRow();
+    jobRow.setRowData(rowData);
+
+    when(jobRowRepository.findTop10ByJobAndAndJobRowStatus(job, JobRowStatus.STAGED))
+        .thenReturn(List.of(jobRow));
+
+    underTest.processChunk(job);
+
+    verifyNoInteractions(rabbitTemplate);
+
+    ArgumentCaptor<JobRow> jobRowArgumentCaptor = ArgumentCaptor.forClass(JobRow.class);
+    verify(jobRowRepository).saveAndFlush(jobRowArgumentCaptor.capture());
+    assertThat(jobRowArgumentCaptor.getValue()).isEqualTo(jobRow);
+    assertThat(jobRowArgumentCaptor.getValue().getJobRowStatus())
+        .isEqualTo(JobRowStatus.PROCESSED_ERROR);
+    assertThat(jobRowArgumentCaptor.getValue().getValidationErrorDescriptions())
+        .isEqualTo(
+            "Column 'POSTCODE' value '%%$*&$£&^$£*' validation error: Value is not alphanumeric");
+  }
+}
