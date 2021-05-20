efbdc1490c7473a9a5ae824ec6d9ed7569a0b184
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-05-20 14:24:45 +0100

    Add moar unit tests

---
+++ src/test/java/uk/gov/ons/census/bulkprocessor/schedule/RowChunkStagerTest.java
@@ -0,0 +1,56 @@
+package uk.gov.ons.census.bulkprocessor.schedule;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.ons.census.bulkprocessor.model.entity.BulkProcess;
+import uk.gov.ons.census.bulkprocessor.model.entity.Job;
+import uk.gov.ons.census.bulkprocessor.model.entity.JobRow;
+import uk.gov.ons.census.bulkprocessor.model.entity.JobRowStatus;
+import uk.gov.ons.census.bulkprocessor.model.repository.JobRowRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RowChunkStagerTest {
+  @Mock private JobRowRepository jobRowRepository;
+
+  @InjectMocks private RowChunkStager underTest;
+
+  @Test
+  public void testStageChunk() {
+    UUID randomFileName = UUID.randomUUID();
+    try (FileOutputStream fos = new FileOutputStream("/tmp/" + randomFileName)) {
+      fos.write("case_id,refusal_type\n".getBytes());
+      fos.write("e932fea8-aa40-4052-b796-12cb2f2517d2,EXTRAORDINARY_REFUSAL\n".getBytes());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    Job job = new Job();
+    job.setBulkProcess(BulkProcess.REFUSAL);
+    job.setFileId(randomFileName);
+
+    underTest.stageChunk(job, new String[] {"case_id", "refusal_type"});
+
+    ArgumentCaptor<List<JobRow>> jobRowArgumentCaptor = ArgumentCaptor.forClass(List.class);
+    verify(jobRowRepository).saveAll(jobRowArgumentCaptor.capture());
+    assertThat(jobRowArgumentCaptor.getValue().size()).isEqualTo(1);
+    assertThat(jobRowArgumentCaptor.getValue().get(0).getJob()).isEqualTo(job);
+    assertThat(jobRowArgumentCaptor.getValue().get(0).getJobRowStatus())
+        .isEqualTo(JobRowStatus.STAGED);
+    assertThat(jobRowArgumentCaptor.getValue().get(0).getOriginalRowLineNumber()).isEqualTo(1);
+    assertThat(jobRowArgumentCaptor.getValue().get(0).getRowData().get("case_id"))
+        .isEqualTo("e932fea8-aa40-4052-b796-12cb2f2517d2");
+    assertThat(jobRowArgumentCaptor.getValue().get(0).getRowData().get("refusal_type"))
+        .isEqualTo("EXTRAORDINARY_REFUSAL");
+  }
+}
---
+++ src/test/java/uk/gov/ons/census/bulkprocessor/schedule/RowStagerTest.java
@@ -0,0 +1,53 @@
+package uk.gov.ons.census.bulkprocessor.schedule;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.ons.census.bulkprocessor.model.entity.Job;
+import uk.gov.ons.census.bulkprocessor.model.entity.JobStatus;
+import uk.gov.ons.census.bulkprocessor.model.repository.JobRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RowStagerTest {
+  @Mock private JobRepository jobRepository;
+
+  @Mock private RowChunkStager rowChunkStager;
+
+  @InjectMocks private RowStager underTest;
+
+  @Test
+  public void testProcessRows() {
+    UUID randomFileName = UUID.randomUUID();
+    try (FileOutputStream fos = new FileOutputStream("/tmp/" + randomFileName)) {
+      fos.write("case_id,refusal_type\n".getBytes());
+      fos.write("e932fea8-aa40-4052-b796-12cb2f2517d2,EXTRAORDINARY_REFUSAL\n".getBytes());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    Job job = mock(Job.class);
+    when(job.getFileId()).thenReturn(randomFileName);
+    when(job.getStagingRowNumber()).thenReturn(180).thenReturn(190).thenReturn(200);
+    when(job.getFileRowCount()).thenReturn(200);
+
+    when(jobRepository.findByJobStatus(JobStatus.STAGING_IN_PROGRESS)).thenReturn(List.of(job));
+
+    underTest.processRows();
+
+    verify(rowChunkStager, times(2)).stageChunk(job, new String[] {"case_id", "refusal_type"});
+
+    verify(job).setJobStatus(JobStatus.PROCESSING_IN_PROGRESS);
+    verify(jobRepository).saveAndFlush(job);
+  }
+}
