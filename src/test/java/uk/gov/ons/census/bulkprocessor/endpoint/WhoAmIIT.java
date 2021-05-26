89d41977d3d010a0118691c61f28ed981994ccf2
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2021-05-26 09:17:21 +0100

    added WhoAmI test

--- src/test/java/uk/gov/ons/census/bulkprocessor/endpoint/WhoAmIIT.java
+++ src/test/java/uk/gov/ons/census/bulkprocessor/endpoint/WhoAmIIT.java
@@ -1,16 +1,23 @@
 package uk.gov.ons.census.bulkprocessor.endpoint;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
+import uk.gov.ons.census.bulkprocessor.model.dto.JobDto;
 
+import javax.persistence.metamodel.Type;
+import java.util.HashMap;
 import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ContextConfiguration
 @ActiveProfiles("test")
@@ -22,13 +29,13 @@ public class WhoAmIIT {
     private int port;
 
 
-//    @Test
-//    public void testWhoAmI() {
-//        String testUrl = "http://localhost:" + port + "/job";
-//        RestTemplate restTemplate = new RestTemplate();
-//        Map<String, String> a;
-//
-//        restTemplate.getForEntity(testUrl, Map<String, String>.class)
-//
-//    }
+    @Test
+    public void testWhoAmI() {
+        String testUrl = "http://localhost:" + port + "/whoami";
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<HashMap> actualJobsResponse = restTemplate.getForEntity(testUrl,  HashMap.class);
+        Map<String, String> whoAmIData = actualJobsResponse.getBody();
+        assertThat(whoAmIData.get("user")).isEqualTo("dummy@fake-email.com");
+    }
 }
